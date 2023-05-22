a2549343c4ebd5f99c030a59d05bd5cef489ee23
Uladzislau Maltsau <wldyslw@outlook.com>
2023-05-22 12:26:31 +0200

    useApartment generalized for multi-building setups

--- utils/useApartment.ts
+++ utils/useApartment.ts
@@ -1,17 +1,19 @@
 import { useContext, useMemo } from 'react';
 import { ComplexInfoContext } from './contexts';
+import type { Apartment } from '@/types';
 
 export default function useApartment(apartmentId: string | null) {
     const complexInfo = useContext(ComplexInfoContext);
 
-    const apartmentInfo = useMemo(() => {
+    const apartmentInfo: Apartment | null = useMemo(() => {
         if (apartmentId === null) {
             return null;
         }
+
         return (
-            complexInfo.data?.buildings[0].apartments.find(
-                (apt) => apt.id === apartmentId
-            ) ?? null
+            complexInfo.data?.buildings
+                .flatMap((b) => b.apartments)
+                .find((apt) => apt.id === apartmentId) ?? null
         );
     }, [apartmentId, complexInfo.data?.buildings]);
 
